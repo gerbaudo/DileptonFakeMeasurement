bcf1e622e5110a0ad5b02d66b9752de45ce520ed
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-01-22 17:09:20 -0800

    cleanup; filter on histonames without retrieving all histos (speedup 1m37s -> 4s)

--- run/python/makeCutflowTable.py
+++ run/python/makeCutflowTable.py
@@ -8,58 +8,91 @@
 import collections, sys, glob
 import ROOT as r
 
-from NavUtils import getAllHistoNames, classifyHistoByName, organizeHistosByType
+from NavUtils import getAllHistoNames, classifyHistoByName, organizeHistosByType, HistoType, HistoNameClassifier
 from SampleUtils import guessSampleFromFilename
 
 r.gROOT.SetBatch(1)
 
+#######
+# begin parameters
+channel = 'ee'            # should be any of ['ee', 'em', 'mm', 'all']
 referenceHisto = 'onebin' # name of the histogram to be used to extract the counts
 referenceSyst = 'NOM'     # name of the syst to be used to extract the counts
+prodTag = 'Jan15_n0115'   # tag of the files to be used
 inputDir = '/export/home/gerbaudo/workarea/Susy2013/SusyTest0/run/anaplots/merged'
-inputFileNames = glob.glob(inputDir+'/'+'*.root')
-print 'input files:\n'+'\n'.join(inputFileNames)
+# end parameters
+#######
+
+
+inputFileNames = glob.glob(inputDir+'/'+'*'+prodTag+'*.root')
 inputFiles = [r.TFile.Open(f) for f in inputFileNames]
+assert len(inputFileNames)==len(inputFiles),"Cannot open some of the input files"
+print 'input files:\n'+'\n'.join(inputFileNames)
 
-
+refHistoType = HistoType(pr='', ch=channel, var=referenceHisto, syst=referenceSyst)
+histoNameClassifier = HistoNameClassifier()
 
 histosByType = collections.defaultdict(list)
 
 for fname, infile in zip(inputFileNames, inputFiles) :
     samplename = guessSampleFromFilename(fname)
-    histoNames = getAllHistoNames(inputFiles[0], onlyTH1=True)
+    histoNames = [n for n in getAllHistoNames(infile, onlyTH1=True)
+                  if refHistoType.matchAllAvailabeAttrs( histoNameClassifier.histoType( n ) )]
     histos = [infile.Get(hn) for hn in histoNames]
     for h in histos : classifyHistoByName(h)
     organizeHistosByType(histosByType, histos, samplename)
 
-refHistos = dict((k, v) for k, v in histosByType.iteritems()
-                 if k.var==referenceHisto and k.syst==referenceSyst)
+refHistos = histosByType # already filtered histonames, all histosByType are refHistos
 allSamples = list(set([h.sample for histos in refHistos.values() for h in histos]))
 allSelects = sorted(list(set([k.pr for k in histosByType.keys()])))
-
-print allSamples
-print allSelects
+print 'allSamples : ',allSamples
+print 'allSelects : ',allSelects
 
 sampleCountsPerSel = dict() # counts[sample][sel]
-channel = 'ee' # ['ee', 'em', 'mm', 'all']
 countsSampleSel = dict([(s, collections.defaultdict(float)) for s in allSamples])
-
-print "collected all the necessary histograms"
 for t, histos in refHistos.iteritems() :
     if t.ch != channel : continue
     for h in histos :
         sample, sel = h.sample, h.type.pr
         countsSampleSel[sample][sel] += h.Integral()
 
-colWidth = 12
+
 endrow = ' \\\\'
+tablePreamble = '% \usepackage{booktabs}\n' \
+                +'% \usepackage{placeins}\n' \
+                +'\\begin{table}[htbp] \n' \
+                + '\\begin{center} \n' \
+                + '\\begin{tabular}{l' + 'r'*len(allSamples) + '} \n' \
+                + '\\toprule '
+tableEpilogue = '\\bottomrule \n' \
+                +'\\end{tabular} \n' \
+                +'\\caption{Add a caption here} \n' \
+                +'\\end{center} \n' \
+                +'\\end{table} \n' \
+                +'\\FloatBarrier \n'
+colWidth = 12
 fwidthField = '%'+str(colWidth)+'s'
 header = ' & '.join([fwidthField % t for t in ['selection']+allSamples]) + endrow
-print header
 
+
+print
+print tablePreamble
+print header
+print '\\midrule'
 for sel in allSelects :
     counts = [countsSampleSel[sam][sel]
-              if sam in countsSampleSel and sel in countsSampleSel[sam] else None
-              for sam in allSamples
-              ]              
+              if sam in countsSampleSel and sel in countsSampleSel[sam] else None \
+              for sam in allSamples ]
     line = ' & '.join([fwidthField % f for f in [sel]+["%.1f" % c for c in counts]]) + endrow
     print line
+print tableEpilogue
+
+#lines = [' & '.join([fwidthField % f
+#                     for f in [sel]+["%.1f" % c
+#                                     for c in counts]]) + endrow
+#         for counts in [countsSampleSel[sam][sel]
+#                        for sam in allSamples
+#                        for sel in allSelects
+#                        if sam in countsSampleSel \
+#                        and sel in countsSampleSel[sam] ] ]
+
