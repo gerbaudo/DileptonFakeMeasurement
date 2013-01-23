1d9a203d742353ecf53931e6f55863220666eeea
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-01-23 14:36:21 -0800

    comment out with '##-' the samples that are not used in runSP.sh

--- run/filelist/datasets.py
+++ run/filelist/datasets.py
@@ -17,33 +17,33 @@ wantedDsets = { # mode : [dsets]
     ##    for ll in ['ee', 'mumu', 'tautau'] for f in ['Heavy', 'Light']]
     # New alternative Z+jets (Yippeee)
     + ["Sherpa_CT10_Z%s" % ll for ll in ['ee', 'mumu', 'tautau']]
-    # Z+jets
-    + ["Z%(ll)sNp%(np)d" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4, 5]]
-    # Zbb + jets
-    + ["Z%(ll)sbbNp%(np)d" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
-    # Zcc + jets
-    + ["Z%(ll)sccNp%(np)d" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
+    ##- # Z+jets
+    ##- + ["Z%(ll)sNp%(np)d" % {'ll':ll, 'np':np}
+    ##-    for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4, 5]]
+    ##- # Zbb + jets
+    ##- + ["Z%(ll)sbbNp%(np)d" % {'ll':ll, 'np':np}
+    ##-    for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
+    ##- # Zcc + jets
+    ##- + ["Z%(ll)sccNp%(np)d" % {'ll':ll, 'np':np}
+    ##-    for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
     # Low mass Z
     + ["Z%(ll)sNp%(np)dExcl_Mll10to60" % {'ll':ll, 'np':np}
        for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4]]
-    # W+Jets (temporary due to bugs)
-    + ["Sherpa_CT10_W%s" % lv for lv in ['enu', 'munu', 'taunu']]
-    # W+jets
-    + ["W%(lv)sNp%(np)d" % {'lv':lv, 'np':np}
-       for lv in ['enu', 'munu', 'taunu'] for np in [0, 1, 2, 3, 4, 5]]
-    # Wbb
-    + ["WbbNp%d" % np for np in [0, 1, 2, 3]]
-    # Wcc
-    + ["WccNp%d" % np for np in [0, 1, 2, 3]]
-    + ["WcNp%d" % np for np in [0, 1, 2, 3]]
+    ##- # W+Jets (temporary due to bugs)
+    ##- + ["Sherpa_CT10_W%s" % lv for lv in ['enu', 'munu', 'taunu']]
+    ##- # W+jets
+    ##- + ["W%(lv)sNp%(np)d" % {'lv':lv, 'np':np}
+    ##-    for lv in ['enu', 'munu', 'taunu'] for np in [0, 1, 2, 3, 4, 5]]
+    ##- # Wbb
+    ##- + ["WbbNp%d" % np for np in [0, 1, 2, 3]]
+    ##- # Wcc
+    ##- + ["WccNp%d" % np for np in [0, 1, 2, 3]]
+    ##- + ["WcNp%d" % np for np in [0, 1, 2, 3]]
     # single top
-    + ["singletop_tchan_%s" % l for l in ['e', 'mu', 'tau']]
+    ##- + ["singletop_tchan_%s" % l for l in ['e', 'mu', 'tau']]
     + ["SingleTopWtChanIncl"]
     # ttbar
-    + ["McAtNloJimmy_CT10_ttbar_LeptonFilter",]
+    ##- + ["McAtNloJimmy_CT10_ttbar_LeptonFilter",]
     + ["Ttbar%s" % ttd for ttd in ["LeptLept", "LeptTaulept", "TauleptTaulept",
                                    "LeptHad", "LeptTauhad", "TauleptHad",
                                    "TauleptTauhad", "HadTauhad", "TauhadTauhad",]]
@@ -60,8 +60,8 @@ wantedDsets = { # mode : [dsets]
     + ['llnunu_SS_EW6', 'llnunujj_SS',]
     # Triboson
     + ['ZWWStar_lllnulnu', 'ZZZStar_nunullll', 'WWWStar_lnulnulnu',]
-    # HF samples
-    + ["%(qq)sTo%(l)s15" % {'qq':qq, 'l':l} for qq in ['bb', 'cc'] for l in ['e', 'mu']]
+    ##- # HF samples
+    ##- + ["%(qq)sTo%(l)s15" % {'qq':qq, 'l':l} for qq in ['bb', 'cc'] for l in ['e', 'mu']]
     ,
     'susy' : []
     + ["wA_noslep_WH_2Lep_%d" % i for i in range(1, 61+1)]  # 2l
