b73e4f6c623cad22d92d694547679c84fc4b01a3
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-01-23 18:28:03 -0800

    cmd-line options

--- run/filelist/submitJobs.py
+++ run/filelist/submitJobs.py
@@ -13,16 +13,27 @@
 # davide.gerbaudo@gmail.com
 # Jan 2013
 
-import os
+import optparse, os
 import datasets
 
-validModes = ['data', 'mc12', 'susy']
+validModes = ['mc12', 'susy', 'data',]
 validOtherOptions = ['', '--1fb', '--AD']
+defaultBatchTag = '_Jan21_n0115'
+defaultOtherOptions = ''
 
-mode = 'susy'
-otherOptions = ''
-batchTag = '_Jan21_n0115'
 
+parser = optparse.OptionParser()
+parser.add_option("-m", "--mode", dest="mode", default=validModes[0],
+                  help="possible modes : %s" % str(validModes))
+parser.add_option("-t", "--tag", dest="tag", default=defaultBatchTag,
+                  help="batch tag (default '%s')" % defaultBatchTag)
+parser.add_option("-O", "--other-opt", dest="otherOptions", default=defaultOtherOptions,
+                  help="other options for qsub script (default '%s', possible values %s)" \
+                  % (defaultOtherOptions, str(validOtherOptions)))
+(options, args) = parser.parse_args()
+mode = options.mode
+batchTag = options.tag
+otherOptions = options.otherOptions
 assert mode in validModes,"Invalid mode %s (should be one of %s)" % (mode, str(validModes))
 assert otherOptions in validOtherOptions, "Invalid otherOptions '%s' (should be one of %s)" % (otherOptions, str(validOtherOptions))
 
