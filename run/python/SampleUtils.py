7a53ac5e5461e70b267df18517d0741fe8b22397
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-01-25 12:18:51 -0800

    guess sample name for WH

--- run/python/SampleUtils.py
+++ run/python/SampleUtils.py
@@ -5,6 +5,7 @@
 # davide.gerbaudo@gmail.com
 # Jan 2013
 
+import re
 import ROOT as r
 
 colors = {
@@ -23,5 +24,9 @@ def guessSampleFromFilename(filename='', verbose=False) :
          or 'WW_' in filename \
          or 'WZ_' in filename : return 'diboson'
     elif 'Wjet_' in filename : return 'wjets'
+    elif 'WH_2Lep' in filename or 'WH_3Lep':
+        match = re.search('(WH_\dLep_\d+)', filename)
+        assert match, "failed to extract the WH pattern from %s"%filename
+        return match.group()
     else :
         if verbose : print "cannot guess samplename for %s" % filename
