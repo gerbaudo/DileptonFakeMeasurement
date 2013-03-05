c71d272aeca5c5582caca60c00f3cb8610829e58
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-05 15:26:04 -0800

    also add the histo for tot mv1

--- Root/SusyPlotter.cxx
+++ Root/SusyPlotter.cxx
@@ -273,7 +273,8 @@ void SusyPlotter::Begin(TTree* /*tree*/)
 	NEWHIST(mt_l1_met,      "m_{T}(l_{1}, met)", nmassbins, massmin, massmax);
 	NEWHIST(mt_l_met_min,   "m_{T}^{min}(l_{1}, met)", nmassbins, massmin, massmax);
 	NEWHIST(mct_top_tag,    "m_{CT} top tag", 2, -0.5, +1.5);
-	NEWHIST(sumJ0J1_mv1tag, "MV1(j0) + MV1(j1)", 25, +0.0, +2.0);
+	NEWHIST(sumJ0J1_mv1tag, "MV1(j0) + MV1(j1)", 50, +0.0, +0.5);
+	NEWHIST(numNeutrinoSol, "Number of neutrino solutions (2l+2j)", 9, -0.5, +8.5);
 
 	NEWHIST2(l0_l1_pt, "l0 vs l1 pt", nptbins, ptmin, ptmax);
 
