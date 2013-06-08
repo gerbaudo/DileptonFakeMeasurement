addce8af42d632a9ad0afc517e8a6a9212fb875d
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-06-08 15:59:24 -0700

    save plots only for final selection; better filename

--- run/python/plotSignificance.py
+++ run/python/plotSignificance.py
@@ -42,10 +42,13 @@ verbose         = options.verbose
 
 countsSigSampleSel = readFromPickle(sigInputFname)
 countsBkgSampleSel = readFromPickle(bkgInputFname)
-
+interestingSelections = ["sr%d"%i for i in range(6,9+1)]
 countBkgTot = collections.defaultdict(float)
 for sample, countsSel in countsBkgSampleSel.iteritems() :
+    if sample in ['data', 'totbkg'] : continue
     for sel, counts in countsSel.iteritems() :
+        if sel not in interestingSelections : continue
+        print 'adding '+sample+' to '+sel+' ('+str(counts)+')'
         countBkgTot[sel] += counts
 print countBkgTot
 
@@ -53,6 +56,7 @@ reqDb = ModeAWhDbReqid()
 parDb = ModeAWhDbPar()
 
 def selIsRelevant(sel) : return sel.startswith('sr')
+def selIsFinal(sel) : return sel in interestingSelections
 
 mc1Range = {'min': min(parDb.allMc1()), 'max' : max(parDb.allMc1())}
 mn1Range = {'min': min(parDb.allMn1()), 'max' : max(parDb.allMn1())}
@@ -63,7 +67,7 @@ for sample, countsSel in countsSigSampleSel.iteritems() :
     mc1, mn1 = parDb.mc1Mn1ByReqid(reqDb.reqidBySample(sample))
     print "%s (%.1f, %.1f) " % (sample, mc1, mn1)
     for sel, counts in sorted(countsSel.iteritems()) :
-        if not selIsRelevant(sel) : continue
+        if not selIsFinal(sel) : continue
         histo = histos[sel] if sel in histos else r.TH2F(sel+'_zn',
                                                          sel+" Z_{n} (#deltab=%.2f);mc_{1};mn_{1}"%backgroundRelErr,
                                                          50, float(mc1Range['min']), float(mc1Range['max']),
@@ -97,7 +101,7 @@ for s, h in histos.iteritems() :
         tex.DrawLatex(1.0-can.GetTopMargin(), 1.0-can.GetRightMargin(), "Signal: x %.1f"%scale)
         can.Update()
     if sigScale != defaultSigScale : writeScale(c, sigScale, h.GetTitleFont())
-    c.SaveAs(s+'.png')
+    c.SaveAs('signif_'+s+'.png')
 
 
 
