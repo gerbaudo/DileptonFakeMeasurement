46de6830081ebf6064a1ef115484422155105bc1
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-06-08 19:48:46 -0700

    add verbose options, drop multiple tags

--- run/python/makeList.py
+++ run/python/makeList.py
@@ -23,11 +23,14 @@ parser.add_option("-s", "--sample-regexp", dest="samples", default='.*',
                   help="create filelists only for matching samples (default '.*'). Example: Alpgen Z+jets -s '^Z(ee|mumu|tautau).*'")
 parser.add_option("-t", "--tag", dest="tag", default=defaultTag,
                   help="production tag (default '%s')" % defaultTag)
+parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
+                  help="print more details about what is going on")
 (options, args) = parser.parse_args()
 mode   = options.mode
 outdir = options.outdir
 regexp = options.samples
 tag    = options.tag
+verbose= options.verbose
 assert mode in validModes,"Invalid mode %s (should be one of %s)" % (mode, str(validModes))
 
 # Directory where files are
@@ -35,23 +38,22 @@ basedir = {'data' : '/gdata/atlas/ucintprod/SusyNt/data12_'+tag+'/', # data
            'mc12' : '/gdata/atlas/ucintprod/SusyNt/mc12_'+tag+'/',   # mc backgrounds
            'susy' : '/gdata/atlas/ucintprod/SusyNt/susy_'+tag+'/',   # mc signals
            }
-tags = [tag]
 wantedDsets = datasets.wantedDsets
 def filterWithRegexp(stringList, regexp) :
     return [d for d in stringList if re.search(regexp, d)]
 wantedDsets = dict([(k, filterWithRegexp(vals, regexp)) for k, vals in wantedDsets.iteritems()])
-###############################################################################################
-#                           Don't need to edit below here!!!                                  #
-###############################################################################################
+
+if verbose :
+    print "Options:\n" \
+          + '\n'.join(["%s : %s" % (o, eval(o)) for o in ['mode','outdir','regexp', 'tag',]])
+
 dlist = []
 dir = basedir[mode]
-for tag in tags :
-    print tag
-    print "ls " + dir + " | grep " + tag + " | grep user"
-    ls = subprocess.Popen(["ls " + dir + " | grep " + tag + " | grep user"],shell=True,stdout=subprocess.PIPE)
-    dlist  = dlist + [l for l in [ll.lstrip().rstrip()
-                                  for ll in (ls.stdout.read()).split("\n")]
-                      if l] # skip empty lines
+cmd = 'ls '+dir+' | grep '+tag+' | grep user'
+if verbose : print cmd
+ls = subprocess.Popen([cmd], shell=True,stdout=subprocess.PIPE)
+dlist  = [l for l in [ll.lstrip().rstrip() for ll in (ls.stdout.read()).split("\n")]
+          if l] # skip empty lines
 
 def contains(dataset, name):
     if (name + ".") in dataset:
@@ -64,7 +66,7 @@ def makeFile(dataset, name):
 
 wanted = wantedDsets[mode]
 for ds in dlist:
-    print ds
+    if verbose : print ds
     for name in wanted:
         if(contains(ds,name) and not ("_a" in ds)):
             makeFile(ds,name)
