10da087fa3af9d89924bea88778b85093a3dd04e
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-06-08 18:44:24 -0700

    uncomment Alpgen Z+jets; update Z<ll>Np<n>Excl names

--- run/filelist/datasets.py
+++ run/filelist/datasets.py
@@ -17,17 +17,17 @@ wantedDsets = { # mode : [dsets]
     ##    for ll in ['ee', 'mumu', 'tautau'] for f in ['Heavy', 'Light']]
     # New alternative Z+jets (Yippeee)
     + ["Sherpa_CT10_Z%s" % ll for ll in ['ee', 'mumu', 'tautau']]
-    ##- # Z+jets
-    ##- + ["Z%(ll)sNp%(np)d" % {'ll':ll, 'np':np}
-    ##-    for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4, 5]]
-    ##- # Zbb + jets
-    ##- + ["Z%(ll)sbbNp%(np)d" % {'ll':ll, 'np':np}
-    ##-    for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
-    ##- # Zcc + jets
-    ##- + ["Z%(ll)sccNp%(np)d" % {'ll':ll, 'np':np}
-    ##-    for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
+    # Z+jets
+    + ["Z%(ll)sNp%(np)d" % {'ll':ll, 'np':np}
+       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4, 5]]
+    # Zbb + jets
+    + ["Z%(ll)sbbNp%(np)d" % {'ll':ll, 'np':np}
+       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
+    # Zcc + jets
+    + ["Z%(ll)sccNp%(np)d" % {'ll':ll, 'np':np}
+       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
     # Low mass Z
-    + ["Z%(ll)sNp%(np)dExcl_Mll10to60" % {'ll':ll, 'np':np}
+    + ["Z%(ll)sNp%(np)dExcl" % {'ll':ll, 'np':np}
        for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4]]
     ##- # W+Jets (temporary due to bugs)
     ##- + ["Sherpa_CT10_W%s" % lv for lv in ['enu', 'munu', 'taunu']]
