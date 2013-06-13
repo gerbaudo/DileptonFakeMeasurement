da78876c0e4d819db9175523128ca5daab2155d3
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-06-12 20:10:44 -0700

    merged fake requid, ModeAWhDbMergedFake2Lreqid

--- run/python/SampleUtils.py
+++ run/python/SampleUtils.py
@@ -85,6 +85,18 @@ class ModeAWhDbReqid :
     def sampleByReqid(self, reqid) :
         return next((s for s,r in self.entries.iteritems() if r == reqid), None)
 
+class ModeAWhDbMergedFake2Lreqid :
+    "provide fake request ids to emulate merged samples in the 2L (mc1,mn1) plane"
+    def __init__(self) :
+        pass
+    def reqidByMc1Mn1(self, mc1, mn1) :
+        x, y = float(mc1), float(mn1)
+        if   y<(-x+210) : return 1765700
+        elif y<(-x+280) : return 1765800 if y>(x-210) else 1765900
+        elif y<(-x+360) : return 1766000 if y>(x-210) else 1766100 if y>(x-280) else 1766200
+        else            : return 1766300
+        #if y<(x-100) else None # this is messing up also the bottom half? later on...
+
 #
 # testing
 #
