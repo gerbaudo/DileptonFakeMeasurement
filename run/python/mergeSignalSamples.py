8f7e5e53a31bdd30ab714dd2f467e149c16c13e8
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-06-13 00:32:56 -0700

    add regexp histo filter

--- run/python/mergeSignalSamples.py
+++ run/python/mergeSignalSamples.py
@@ -1,9 +1,9 @@
 #!/bin/env python
 
-# produce a latex cutflow table from the histograms produced by SusyPlot
+# merge signal samples to increase the signal stats
 #
 # davide.gerbaudo@gmail.com
-# Jan 2013
+# Jun 2013
 
 import collections
 import glob
@@ -27,6 +27,7 @@ class SamplesMerger :
         self.reqDb = ModeAWhDbReqid()
         self.overwrite = False
         self.verbose = False
+        self.regexHist = '.*'
     def idTagFromFilename(self, fname) :
         'parse something like wA_noslep_WH_2Lep_9_May16_n0139.AnaHists'
         match = re.search('wA_noslep_WH_2Lep_(?P<id>\d+?)_(?P<tag>.*?).AnaHist', fname)# nongreedy
@@ -40,9 +41,9 @@ class SamplesMerger :
     def mergeAndWrite(self, outdir) :
         """write merged histos, unless !overwrite, in which case just
         compute merged coords and fnames"""
-        def mergeFiles(targetFile, filenames) :
+        def mergeFiles(targetFile, filenames, regex) :
             infiles = [r.TFile.Open(f) for f in filenames]
-            histonames = getAllHistoNames(infiles[0])
+            histonames = [h for h in getAllHistoNames(infiles[0]) if re.search(regex, h)]
             scale = 1.0/len(infiles)
             outFile = r.TFile.Open(targetFile, 'recreate')
             outFile.cd()
@@ -72,7 +73,7 @@ class SamplesMerger :
             if self.verbose : print str(point)+' : '+outfname
             skipMerge = os.path.exists(outfname) and not self.overwrite
             if  skipMerge : continue
-            else          : mergeFiles(outfname, fnames)
+            else          : mergeFiles(outfname, fnames, self.regexHist)
     def printMergedFiles(self) :
         for rid in self.outFnameByReqid.keys() :
             print self.mergedPointsByReqid[rid], ' : ', self.outFnameByReqid[rid]
@@ -85,16 +86,19 @@ if __name__=='__main__' :
     For now, only working for WH_2Lep.
     Examples:
     > ./python/mergeSignalSamples.py inputdir/ outputdir/ tag
+    > ./python/mergeSignalSamples.py anaplots/ mergedSignals/ Jun06_n0139 -v -r '.*onebin.*'
     """
     parser = optparse.OptionParser(usage=usage)
     parser.add_option('-o', '--overwrite', action='store_true', dest='overwrite', default=False,
                       help='overwrite existing files')
+    parser.add_option('-r', '--regex-histo', dest='regexhist', default='.*',
+                      help='only merge the histos whose name matches regexp (runs faster)')
     parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False,
                       help='print more details about what is going on')
     (options, args) = parser.parse_args()
     if len(args)!=3 : parser.error('incorrect number of arguments')
     inputDir, outputDir, tag = args[0], args[1], args[2]
-    overwrite, verbose = options.overwrite, options.verbose
+    overwrite, regexHist, verbose = options.overwrite, options.regexhist, options.verbose
     
     inputFileNames = glob.glob(inputDir+'/'+'*WH_2Lep*'+tag+'*.root')
     if not os.path.isdir(outputDir) : os.mkdir(outputDir)
@@ -105,7 +109,7 @@ if __name__=='__main__' :
         print 'Input files:\n'+'\n'.join(inputFileNames)
     
     merger = SamplesMerger()
-    if verbose : merger.verbose = verbose
+    merger.regexHist, merger.verbose = regexHist, verbose
     for f in inputFileNames : merger.addFile(f)
     if overwrite : merger.overwrite = True
     merger.mergeAndWrite(outputDir) 
