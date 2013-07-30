456c87c25cdb117a0da446223ba5af262c51d901
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-07-30 05:46:31 -0700

    drop obsolete wantedDsets, improve description

--- run/python/datasets.py
+++ run/python/datasets.py
@@ -1,73 +1,14 @@
 #!/bin/env python
 
-# List of datastets organized in a dict (imported from Matt, Jan 2013)
+# List of dataset definitions
+#
+# Details:
+# see the Dataset class for more info on their attributes.
+# Datasets can also be defined without being used; search for
+# 'placeholder' to see the disabled samples.
 #
 # davide.gerbaudo@gmail.com
 # Jan 2013
-
-# What files we want, with appropriate names
-wantedDsets = { # mode : [dsets]
-    'data' : []
-    + ["period%(p)s.physics_%(s)s" % {'p':p, 's':s}
-       for p in ['A', 'B', 'C', 'D', 'E', 'G', 'H', 'I', 'J', 'L'] for s in ['Egamma', 'Muons']]
-    ,
-    'mc12' : []
-    ## # Alternative Z+jets AFII samples
-    ## + ["Z%(ll)s%(f)%sJets_AF2" % {'ll':ll, 'f':f}
-    ##    for ll in ['ee', 'mumu', 'tautau'] for f in ['Heavy', 'Light']]
-    ## # New alternative Z+jets (Yippeee)
-    ## + ["Sherpa_CT10_Z%s" % ll for ll in ['ee', 'mumu', 'tautau']]
-    # Z+jets
-    + ["AlpgenPythia_P2011C_Z%(ll)sNp%(np)d" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4, 5]]
-    # Zbb + jets
-    + ["AlpgenPythia_P2011C_Z%(ll)sbbNp%(np)d" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
-    # Zcc + jets
-    + ["AlpgenPythia_P2011C_Z%(ll)sccNp%(np)d" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3]]
-    # Low mass Z
-    + ["AlpgenPythia_P2011C_Z%(ll)sNp%(np)dExcl" % {'ll':ll, 'np':np}
-       for ll in ['ee', 'mumu', 'tautau'] for np in [0, 1, 2, 3, 4]]
-    ##- # W+Jets (temporary due to bugs)
-    ##- + ["Sherpa_CT10_W%s" % lv for lv in ['enu', 'munu', 'taunu']]
-    ##- # W+jets
-    ##- + ["W%(lv)sNp%(np)d" % {'lv':lv, 'np':np}
-    ##-    for lv in ['enu', 'munu', 'taunu'] for np in [0, 1, 2, 3, 4, 5]]
-    ##- # Wbb
-    ##- + ["WbbNp%d" % np for np in [0, 1, 2, 3]]
-    ##- # Wcc
-    ##- + ["WccNp%d" % np for np in [0, 1, 2, 3]]
-    ##- + ["WcNp%d" % np for np in [0, 1, 2, 3]]
-    # single top
-    ##- + ["singletop_tchan_%s" % l for l in ['e', 'mu', 'tau']]
-    + ["SingleTopWtChanIncl"]
-    # ttbar
-    ##- + ["McAtNloJimmy_CT10_ttbar_LeptonFilter",]
-    + ["Ttbar%s" % ttd for ttd in ["LeptLept", "LeptTaulept", "TauleptTaulept",
-                                   "LeptHad", "LeptTauhad", "TauleptHad",
-                                   "TauleptTauhad", "HadTauhad", "TauhadTauhad",]]
-    # + ["PowhegPythia_AUET2BCT10_ttbar_LeptonFilter_AF2",]
-    + ['McAtNloJimmy_CT10_ttbar_LeptonFilter']
-    + ["ttbar%s" % ttX for ttX in ["Z", "Zj", "W","Wj",]]
-    # diboson
-    + ["lllnu_WZ", "llll_ZZ", "llnunu_ZZ", "llnunu_WW",]
-    + ["WpWm%s%s" % (lv1, lv2)
-       for lv1 in ['enu', 'munu', 'taunu'] for lv2 in ['enu', 'munu', 'taunu']]
-    + ["%sgammaPt10" % lv for lv in  ['enu', 'munu', 'taunu',]]
-    + ["%sgammaPt10" % ll for ll in ['ee', 'mumu', 'tautau',]]
-    # MISSING DIBOSON!!!!!
-    + ["ZZ%s" % l4 for l4 in ['4lep', '4e', '4mu', '2e2mu',]]
-    + ['llnunu_SS_EW6', 'llnunujj_SS',]
-    # Triboson
-    + ['ZWWStar_lllnulnu', 'ZZZStar_nunullll', 'WWWStar_lnulnulnu',]
-    ##- # HF samples
-    ##- + ["%(qq)sTo%(l)s15" % {'qq':qq, 'l':l} for qq in ['bb', 'cc'] for l in ['e', 'mu']]
-    ,
-    'susy' : []
-    + ["wA_noslep_WH_2Lep_%d" % i for i in range(1, 61+1)]  # 2l
-    + ["wA_noslep_WH_3Lep_%d" % i for i in range(1, 66+1)]  # 3l
-    }
 
 def rzip(*iterables) :
     """rigid zip: ensures input iterables have the same length.
@@ -92,7 +33,7 @@ class Dataset :
 
 datasets = []
 sampleType, group, process = None, None, None
-placeholder = True # search for 'placeholder' to see the disabled samples
+placeholder = True
 
 
 sampleType = 'data'
