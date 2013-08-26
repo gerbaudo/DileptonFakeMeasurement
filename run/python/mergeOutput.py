11a5ff281a7bf384a60b3d08e0fb04a336cf74cb
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-08-26 02:01:18 -0700

    move guessLatestTagFromLatestRootFiles to utils

--- run/python/mergeOutput.py
+++ run/python/mergeOutput.py
@@ -12,24 +12,7 @@ import os
 import re
 import subprocess
 from datasets import datasets
-from utils import getCommandOutput
-
-def findLatestOneOrTwoRootFiles(dir) :
-    files = filter(os.path.isfile, glob.glob(dir + "*.root"))
-    files.sort(key=lambda x: os.path.getmtime(x))
-    return files[-2:] if len(files)>=2 else files
-def guessLatestTagFromLatestRootFiles(dir, debug) :
-    "Latest tag if there are at least 2 root files; otherwise empty string"
-    files = [f.replace('.root','').rstrip() for f in findLatestOneOrTwoRootFiles(dir)]
-    def commonSuffix(ll) : return os.path.commonprefix([l[::-1] for l in ll])[::-1]
-    suffix = commonSuffix(files)
-    if not len(suffix) : return ''
-    def cleanupSampleName(suff) :
-        "if the suffix includes part of a sample name, it can be isolated by the ."
-        lastDot = max(0, suffix.rfind('.'))
-        return suffix[suffix.find('_', lastDot):]
-    if debug : print "guessLatestTagFromLatestRootFiles: cleanup '%s'"%suffix
-    return cleanupSampleName(suffix) if len(suffix) else suffix
+from utils import getCommandOutput, guessLatestTagFromLatestRootFiles
 
 usage="""%prog [options] dir
 Merge root files from samples belonging to the same group.
--- run/python/utils.py
+++ run/python/utils.py
@@ -5,6 +5,8 @@
 # davide.gerbaudo@gmail.com
 # 2013-07-25
 
+import glob
+import os
 import re
 import subprocess
 
@@ -16,3 +18,21 @@ def getCommandOutput(command):
 
 def filterWithRegexp(stringList, regexp) :
     return [d for d in stringList if re.search(regexp, d)]
+
+def findLatestOneOrTwoRootFiles(dir) :
+    files = filter(os.path.isfile, glob.glob(dir + "*.root"))
+    files.sort(key=lambda x: os.path.getmtime(x))
+    return files[-2:] if len(files)>=2 else files
+
+def guessLatestTagFromLatestRootFiles(dir, debug) :
+    "Latest tag if there are at least 2 root files; otherwise empty string"
+    files = [f.replace('.root','').rstrip() for f in findLatestOneOrTwoRootFiles(dir)]
+    def commonSuffix(ll) : return os.path.commonprefix([l[::-1] for l in ll])[::-1]
+    suffix = commonSuffix(files)
+    if not len(suffix) : return ''
+    def cleanupSampleName(suff) :
+        "if the suffix includes part of a sample name, it can be isolated by the ."
+        lastDot = max(0, suffix.rfind('.'))
+        return suffix[suffix.find('_', lastDot):]
+    if debug : print "guessLatestTagFromLatestRootFiles: cleanup '%s'"%suffix
+    return cleanupSampleName(suffix) if len(suffix) else suffix
