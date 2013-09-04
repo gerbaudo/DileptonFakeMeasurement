8bd8bc085881377247053a8322c8541e07bd1c12
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-09-04 10:44:51 -0700

    plotter: for now only look at SS region

--- Root/SusyPlotter.cxx
+++ Root/SusyPlotter.cxx
@@ -219,17 +219,26 @@ Bool_t SusyPlotter::Process(Long64_t entry)
   }
   selectObjects();
   if(!selectEvent())    return kTRUE;
-  // DG 26Feb : this needs to be understood.
-  //--DG-- if( nt.evt()->isMC && !isTrueDilepton(m_signalLeptons) ) return kTRUE;
 
   bool count(true);
   bool includeBTag(true), includeTrig(true);
   const Met*          m = m_met;
   const JetVector&    j = m_signalJets2Lep;
   const LeptonVector& l = m_signalLeptons;
+  LeptonVector&     ncl = m_signalLeptons; // non-const leptons: can be modified by qflip
+  Met ncmet(*m_met); // non-const met
+  const TauVector&    t = m_signalTaus;
+  if(!passNlepMin(l, 2)) return false;
   float weight = SusySelection::getEvtWeight(l, includeBTag, includeTrig);
   fillHistos(l, j, m, weight, PR_NONE);
+  m_ET = getDiLepEvtType(l);
+  const DiLepEvtType ll(m_ET), ee(ET_ee), em(ET_em), me(ET_me), mm(ET_mm);
+  bool passSrSS(SusySelection::passSrSs(m_ET, WH_SRSS1, ncl, t, j, m));
+  //SusySelection::passSrSs(m_ET, WH_SRSS1, l, t, j, m));
 
+  if(passSrSS && (ll==ee||ll==mm)) fillHistos(ncl, j, m, weight, PR_SR8);
+  if(passSrSS && (ll==em||ll==me)) fillHistos(ncl, j, m, weight, PR_SR9);
+  /*
   if( passSR6base     (l, j, m)       ) fillHistos(l, j, m, weight, PR_SR6base);
   if( passSR6         (l, j, m, count)) fillHistos(l, j, m, weight, PR_SR6);
   if( passSR8base     (l, j, m)       ) fillHistos(l, j, m, weight, PR_SR8base);
@@ -243,7 +252,7 @@ Bool_t SusyPlotter::Process(Long64_t entry)
   if( passSR7NjPtTot  (l, j, m)       ) fillHistos(l, j, m, weight, PR_SR7NjPtTot);
   if( passSR7NjMll    (l, j, m)       ) fillHistos(l, j, m, weight, PR_SR7NjMll);
   if( passSR7         (l, j, m, count)) fillHistos(l, j, m, weight, PR_SR7);
-
+  */
   return kTRUE;
 }
 //-----------------------------------------
