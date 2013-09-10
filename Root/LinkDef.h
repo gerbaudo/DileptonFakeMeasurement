b0c6b4648f597f3cd01b9623a2000f177a7a98c9
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-09-10 11:58:00 -0700

    swap include order (not sure it is needed)

--- Root/LinkDef.h
+++ Root/LinkDef.h
@@ -2,6 +2,7 @@
 #include "SusyTest0/SusySelection.h"
 #include "SusyTest0/SusyPlotter.h"
 #include "SusyTest0/TightProbability.h"
+#include "SusyTest0/SusySelectionMatt.h"
 #include "SusyTest0/MeasureFakeRate2.h"
 
 #ifdef __CINT__
@@ -15,5 +16,6 @@
 #pragma link C++ class SusySelection;
 #pragma link C++ class SusyPlotter;
 #pragma link C++ class TightProbability;
+#pragma link C++ class SusySelectionMatt;
 #pragma link C++ class MeasureFakeRate2;
 #endif
