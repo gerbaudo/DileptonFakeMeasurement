9ee35d3dc832d77f2d1121b8b3bcfb662597cb87
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-09-27 19:40:32 -0700

    cleanup options, comments; vertical compression

--- util/FinalFakeHist.cxx
+++ util/FinalFakeHist.cxx
@@ -1,75 +1,41 @@
+#include "SusyTest0/FinalNewFake.h"
 
-#include <cstdlib>
+#include <iostream>
 #include <string>
 
 #include "Cintex/Cintex.h"
 
-//#include "SusyTest0/FinalFake.h"
-#include "SusyTest0/FinalNewFake.h"
-//#include "SusyTest0/FinalChannelSepFake.h"
-
 using namespace std;
-
-/*
-
-Plotting Macro
-
-*/
 
 void help()
 {
-  cout << "  Options:"                            << endl;
-
-  cout << "  -n number of events to process"      << endl;
-  cout << "     defaults: -1 (all events)"        << endl;
-
-  cout << "  -o Specify output file name"         << endl;
-  cout << "     default is 'fakeout'"         << endl;
-
-  cout << "  -h print this help"                << endl;
+  cout<<"  Options:"            << endl;
+  cout<<"  -d debuglevel"       << endl;
+  cout<<"  -o outfile.root"     << endl;
+  cout<<"  -h print this help"  << endl;
 }
-
 
 int main(int argc, char** argv)
 {
   ROOT::Cintex::Cintex::Enable();
-
   int dbg = 0;
   string output = "fakeout";
-  
-  cout << "Final Fake Rate" << endl;
-  cout << endl;
-
-  /** Read inputs to program */
   for(int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-d") == 0)
-      dbg = atoi(argv[++i]);
-    else if (strcmp(argv[i], "-o") == 0)
-      output = argv[++i];
-    else
-      {
-	help();
-	return 0;
-      }
+    string opt(argv[i]);
+    if      (opt=="-d")    dbg = atoi(argv[++i]);
+    else if (opt=="-o") output = argv[++i];
+    else if (opt=="-h") { help(); return 0; }
+    else { cout<<"invalid option '"<<opt<<"'"<<endl; help(); return 0; }
   }
-  
-    cout << "flags:" << endl;
-  cout << "  dbg                      " << dbg         << endl;
-  cout << "  output file:             " << output      << endl;
-  cout << endl;
-  
 
-  // Create instance of the class:
-  //FinalFake* format = new FinalFake(output);
-  FinalNewFake* format = new FinalNewFake(output);
-  //FinalChannelSepFake* format = new FinalChannelSepFake(output);
-  format->setDebug(dbg);
-  format->buildRates();
-  
-  cout << endl;
-  cout << "Plotting job done" << endl;
+  cout<<"options:"<<endl;
+  cout<<"\t dbg          : "<<dbg   <<endl;
+  cout<<"\t output file  : "<<output<<endl;
+  cout<<endl;
 
-  delete format;
+  FinalNewFake format(output);
+  format.setDebug(dbg);
+  format.buildRates();
 
   return 0;
 }
