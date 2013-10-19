8fec712633e280a613b6552be210eeeae3cef75a
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-10-18 20:23:14 -0700

    email_me_when_youre_done, initial version

---
+++ run/python/email_me_when_youre_done.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+# email me when all my jobs on the queue are done
+#
+# davide.gerbaudo@gmail.com
+# Oct 2013
+
+import optparse
+import os
+from time import sleep
+from utils import getCommandOutput
+
+def main() :
+    parser = optparse.OptionParser()
+    parser.add_option('--email',        default='davide.gerbaudo@gmail.com')
+    parser.add_option('--message',      default='all jobs done, bam!')
+    parser.add_option('--polleverysec', default=60*5, type='int')
+    parser.add_option('--subject',      default='All jobs done')
+    parser.add_option('--user',         default=os.environ['USER'])
+    (opts, args) = parser.parse_args() 
+    receiver  = opts.email
+    username  = opts.user
+    message   = opts.message
+    subject   = opts.subject
+    everyNsec = opts.polleverysec
+    nChecksDone = 0
+    countJobsCmd="qstat -u %(user)s | grep %(user)s | wc -l"% {'user':username}
+    nJobsRunning = int(getCommandOutput(countJobsCmd)['stdout'])    
+    while nJobsRunning > 0 :
+        sleep(everyNsec)
+        nJobsRunning = int(getCommandOutput(countJobsCmd)['stdout'])
+        nChecksDone += 1
+    message = message+("\n(after %(nchk)d checks every %(nsec)d sec)"
+                       %{'nchk' : nChecksDone, 'nsec' : everyNsec})
+    mailCmd = ("echo \"%(msg)s\" | mail -s \"%(sbj)\" %(dest)s"
+               % {'dest' : receiver, 'msg' : message, 'sbj':subject})
+    getCommandOutput(mailCmd)
+
+if __name__=='__main__' :
+    main()
