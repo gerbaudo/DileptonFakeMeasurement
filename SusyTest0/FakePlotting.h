54ac387d0e423c9572bb9c12cb2279c9fec1eace
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-10-23 22:47:33 -0700

    FakePlotting: major cleanup; drop everything except DataMCSF

--- SusyTest0/FakePlotting.h
+++ SusyTest0/FakePlotting.h
@@ -45,18 +45,7 @@ typedef unsigned int uint;
 
 enum RunOption
 {
-  RO_Data = 0,
-  RO_MC,
-  RO_DataMC,
-  RO_DataMCSub,
-  RO_GJetCR,
   RO_DataMCSF,
-  RO_SRRates,
-  RO_SRComp,
-  RO_TLInfo,
-  RO_SRDump,
-  RO_HFNorm,
-  RO_DumpPer,
   RO_N
 };
 
@@ -77,91 +66,33 @@ class FakePlotting : public myHist
   FakePlotting& setInputDir(const std::string &dir);
   FakePlotting& setOuputDir(const std::string &dir);
   FakePlotting& setInputItercorrFile(const std::string &name);
-  // The various looping functions
-  void MCFakeRate();
-  void DataFakeRate();
-  void DataRateMCSub();
-  void GammaJetCRRates();
-  void DataMCSF(RunOption ro);
-  void MCSRRates();
-  void Composition();
-  void TLPlots();
-  void dumpSRTable();
-  void GetHFNorm();
+  void DataMCSF();
 
-  // Methods to dump signal region fake
-  void dumpSRFake();
   float getBC(TFile* f, string plot, int bin=1){
     return ((TH1F*) f->Get(plot.c_str()))->GetBinContent(bin);
   };
   float getBE(TFile* f, string plot, int bin=1){
     return ((TH1F*) f->Get(plot.c_str()))->GetBinError(bin);
   };
-  float getNewStat(TFile* f, string sr, string ch);
-
-  // Checking for the stat error
-  void checkPercentages();
-
-  // Plotting functions
-  void plotFake(TH1F* h[], vector<string> names, TCanvas* c, 
-		float* MinMax, float* xLeg, float* yLeg,
-		vector<Label> lbls, string save, bool restrictMax=true,
-		bool logy = false);
-  void plotSF(TH1F* h[], vector<string> names, TCanvas* c, 
-	      float* MinMax, float* xLeg, float* yLeg,
-	      vector<Label> lbls, string save, bool doFit = true,
-	      bool topBot = true /*data/MC = true, MC/Data=false*/,
-	      string topLabel = "Rate", string ratLabel = "Data/MC");
-
-  void plotStack(TH1F* h[], vector<string> names, TCanvas* c, 
-		 float* xLeg, float* yLeg,
-		 vector<Label> lbls, string save, bool logy = false);
-
-  // Build Fake histogram
-  TH1F* buildRate(TFile* file, string name, string sname, string xtitle, 
-		  string ytitle, int color, int mark=20);
-  TH1F* buildLFRate(string lepton, string variable);
-  TH1F* buildLFWjetRate(string lepton, string variable);
+  void plotSF(TH1F* h[], vector<string> names, TCanvas* c,
+              float* MinMax, float* xLeg, float* yLeg,
+              vector<Label> lbls, string save, bool doFit = true,
+              bool topBot = true /*data/MC = true, MC/Data=false*/,
+              string topLabel = "Rate", string ratLabel = "Data/MC");
+  TH1F* buildRate(TFile* file, string name, string sname, string xtitle,
+                  string ytitle, int color, int mark=20);
 
   // Build RE histogram doing sideband subtraction
-  TH1F* buildSideBandSubRate(TFile* file, string lep, string var, string xtitle, 
-			     string ytitle, int color, int mark=20);
-  TH1F* buildCorrectedRealRate(TFile* file, string lep, string var, string xtitle, 
-			       string ytitle, int color, int mark=20);
-
-  // Build Fractions
+  TH1F* buildSideBandSubRate(TFile* file, string lep, string var, string xtitle,
+                             string ytitle, int color, int mark=20);
   vector<float> buildFraction(vector<File> files, string plotname);
-
-  // Get Corrected Fake Rate
-  TH1F* getCorrectedRate(TFile* data, TFile* mc, string dataname, string mcname,
-			 string xtitle, int color, int mark = 20);
-
-  // Wrapper to make a lebel struct
-  Label makeLabel(string name, float xpos, float ypos){
-    Label l;
-    l.lbl = name;
-    l.x = xpos;
-    l.y = ypos;
-    return l;
-  };
-
-  // Make stacked histogram
-  THStack* buildStack(TH1F* h[], int nhists);
-
-  // Set the maximum for histogram
   float getMaximum(TH1F* h[], uint nhisto);
   float getMinimum(TH1F* h[], uint nhisto);
-  
-  // Flag for debuggin'
   void setDebug(int dbg){ m_dbg = dbg; };
-
-  // Dump contents of a histogram
   void dumpHisto(TH1F* h);
   static bool isCanvasWithNeededSf(const std::string &canvasName);
  protected:
-  
   vector<File> m_files;
-
   File data;
   File totMC;
   File ttbar;
@@ -173,17 +104,13 @@ class FakePlotting : public myHist
   File gjet;
   File fakePred;
   File HF;
-
   int m_dbg;
-  
   RunOption m_runopt;
-
   FakeStatTool::StatErrorTool* m_fakeStat;
   std::string m_tag;
   std::string m_inputdir;
   std::string m_outputdir;
   std::string m_inputItercorrFile;
-
 };
 
 
--- util/FakePlot.cxx
+++ util/FakePlot.cxx
@@ -22,26 +22,15 @@ Plotting Macro
 
 void help(const vector<string> &opts)
 {
-  size_t i=0;
   cout << "  Options:"                             << endl;
   cout << "  -i inputdir (required)"               << endl;
   cout << "  -t tag (required, e.g. '_Sep_23')"    << endl;
   cout << "  -c itecorrfile(required for some mode)"<< endl;
   cout << "  -o outputdir (required)"              << endl;
   cout << "  -r run option number"                 << endl;
+  size_t i=0;
   size_t width(24);
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : Data (default)"                 << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : MC"                             << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : Data and MC"                    << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : Data - MC Rate"                 << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : Photon + jet"                   << endl; i++;
   cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : Data/MC scale factors"          << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : MC Signal Regions rates"        << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : MC Composition in SR"           << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : TT/TL/LT/LL Matrix Pred Plots"  << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : dump Matrix Pred in SR"         << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : HF normalization"               << endl; i++;
-  cout<<"  "<<setw(3)<<i<<" "<<setw(width)<<opts[i]<<" : Dump Percentages"               << endl; i++;
   cout << "  -h print this help"                << endl;
   cout<<endl
       <<"Example:"<<endl
@@ -58,22 +47,10 @@ int main(int argc, char** argv)
 
   int dbg = 0;
   string inputdir, inputItecorrfile, outputdir, tag;
-  RunOption ro = RO_Data;
+  RunOption ro = RO_DataMCSF;
 
   vector<string> txtOptions; // ugly duplication, but allows for mnemonic...fixme
-  txtOptions.push_back("data"            );
-  txtOptions.push_back("mc"              );
-  txtOptions.push_back("data-and-mc"     );
-  txtOptions.push_back("data-mc-rate"    );
-  txtOptions.push_back("photon-jet"      );
   txtOptions.push_back("datamc-sf"       );
-  txtOptions.push_back("mc-sr-rates"     );
-  txtOptions.push_back("mc-sr-comp"      );
-  txtOptions.push_back("matrix-pred-plot");
-  txtOptions.push_back("matrix-pred-dump");
-  txtOptions.push_back("hf-norm"         );
-  txtOptions.push_back("percent-dump"    );
-
   cout << "FakePlot" << endl;
   cout << endl;
 
@@ -102,7 +79,7 @@ int main(int argc, char** argv)
   if(unspecifiedInputdir) { cout<<"inputdir required." <<endl; help(txtOptions); return 0; }
   if(unspecifiedOutdir)   { cout<<"outputdir required."<<endl; help(txtOptions); return 0; }
 
-  bool corrFileIsRequired(ro==RO_DataMCSF || ro==RO_DataMC);
+  bool corrFileIsRequired(ro==RO_DataMCSF);
   if(corrFileIsRequired && unspecifiedCorr) {
     cout<<"inputItecorrfile required. (example 'corFake_Sep11_2013_forDavide.root')"<<endl;
     help(txtOptions);
@@ -128,19 +105,8 @@ int main(int argc, char** argv)
   plot->setInputItercorrFile(inputItecorrfile);
   plot->init();
 
-  if(ro == RO_MC)        plot->MCFakeRate();
-  if(ro == RO_Data)      plot->DataFakeRate();
-  if(ro == RO_DataMCSub) plot->DataRateMCSub();
-  if(ro == RO_GJetCR)    plot->GammaJetCRRates();
-  if(ro == RO_DataMCSF)  plot->DataMCSF(ro);
-  if(ro == RO_DataMC)    plot->DataMCSF(ro);
-  if(ro == RO_SRRates)   plot->MCSRRates();
-  if(ro == RO_SRComp){   /*plot->Composition();*/ plot->dumpSRTable(); }
-  if(ro == RO_TLInfo)    plot->TLPlots();
-  if(ro == RO_SRDump)    plot->dumpSRFake();
-  if(ro == RO_HFNorm)    plot->GetHFNorm();
-  if(ro == RO_DumpPer)   plot->checkPercentages();
-
+  if(ro == RO_DataMCSF)  plot->DataMCSF();
+  else cout<<"Obsolete or unsupported option"<<endl;
   cout << endl;
   cout << "Plotting job done" << endl;
 
