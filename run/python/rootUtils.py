419df502785686c50fd37065e6ba59c4643c23b3
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-11-05 00:52:23 -0800

    rootUtils: addnl args referenceLine, topRightLegend

--- run/python/rootUtils.py
+++ run/python/rootUtils.py
@@ -10,9 +10,8 @@ import ROOT as r
 r.gROOT.SetBatch(True)                     # no windows popping up
 r.PyConfig.IgnoreCommandLineOptions = True # don't let root steal our cmd-line options
 
-def referenceLine(xmin=0., xmax=100.0) :
-    y1, y2 = 1.0, 1.0
-    l1 = r.TLine(xmin, y1, xmax, y2)
+def referenceLine(xmin=0., xmax=100.0, ymin=1.0, ymax=1.0) :
+    l1 = r.TLine(xmin, ymin, xmax, ymax)
     l1.SetLineStyle(3)
     l1.SetLineColor(r.kGray+1)
     return l1
@@ -25,9 +24,12 @@ def unitLineFromFirstHisto(histos) :
     xAx = fH.GetXaxis()
     return referenceLine(xAx.GetXmin(), xAx.GetXmax())
 
-def topRightLegend(pad,  legWidth, legHeight) :
+def topRightLegend(pad,  legWidth, legHeight, shift=0.0) :
     rMarg, lMarg, tMarg = pad.GetRightMargin(), pad.GetLeftMargin(), pad.GetTopMargin()
-    leg = r.TLegend(1.0 - rMarg - legWidth, 1.0 - tMarg - legHeight, 1.0 - rMarg, 1.0 - tMarg)
+    leg = r.TLegend(1.0 - rMarg - legWidth + shift,
+                    1.0 - tMarg - legHeight + shift,
+                    1.0 - rMarg + shift,
+                    1.0 - tMarg + shift)
     leg.SetBorderSize(1)
     leg.SetFillColor(0)
     leg.SetFillStyle(0)
