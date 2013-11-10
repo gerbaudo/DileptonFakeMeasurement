a0be4a3e62f27d16fa0ad04c144d3443903fd6b4
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-11-09 23:54:46 -0800

    binning: avoid hardcoded nbins
    Details:
    compute it from array length.
    Amend to previous commit message: The bin content of all remaining
    histograms is constent with the old code.

--- SusyTest0/FakeBinnings.h
+++ SusyTest0/FakeBinnings.h
@@ -8,18 +8,18 @@
 
 namespace susy {
 namespace fake {
-const float FakePtbins[] = {10,20,35,100};
-const int   nFakePtbins   = 3;
+const float FakePtbins[]       = {10,20,35,100};
 const float coarseFakePtbins[] = {10,20,35,100};
-const int  nCoarseFakePtbins   = 3;
-const float Etabins[] = {0,1.37,1.52,2.1,2.5};
-const int  nEtabins = 4;
-const float CoarseEtabins[] = {0,1.37,2.5};
-const int  nCoarseEtabins = 2;
-const float Metbins[] = {0, 20, 30, 40, 50, 60, 100, 200};
-const int   nMetbins  = 7;
-const float Jetbins[] = {-0.5, 0.5, 1.5, 2.5, 3.5};
-const float nJetbins  = 4;
+const float Etabins[]          = {0,1.37,1.52,2.1,2.5};
+const float CoarseEtabins[]    = {0,1.37,2.5};
+const float Metbins[]          = {0, 20, 30, 40, 50, 60, 100, 200};
+const float Jetbins[]          = {-0.5, 0.5, 1.5, 2.5, 3.5};
+const int   nFakePtbins        = sizeof(FakePtbins)       / sizeof(FakePtbins[0])       - 1;
+const int   nCoarseFakePtbins  = sizeof(coarseFakePtbins) / sizeof(coarseFakePtbins[0]) - 1;
+const int   nEtabins           = sizeof(Etabins)          / sizeof(Etabins[0])          - 1;
+const int   nCoarseEtabins     = sizeof(CoarseEtabins)    / sizeof(CoarseEtabins[0])    - 1;
+const int   nMetbins           = sizeof(Metbins)          / sizeof(Metbins[0])          - 1;
+const int   nJetbins           = sizeof(Jetbins)          / sizeof(Jetbins[0])          - 1;
 } // end namespace fake
 } // end namespace susy
 
