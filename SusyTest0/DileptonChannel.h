db100fb9a03eaad1030ee3216fe6377effe2e506
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-12-05 18:41:26 -0800

    avoid 'using namespace std' in headers; include std::string where needed

--- SusyTest0/DileptonChannel.h
+++ SusyTest0/DileptonChannel.h
@@ -2,13 +2,13 @@
 #ifndef SUSY_WH_DILEPTONCHANNEL_H
 #define SUSY_WH_DILEPTONCHANNEL_H
 
-// dilepton channel
+#include <string>
 
 namespace susy {
 namespace wh {
 
 enum Chan                   { Ch_all = 0, Ch_ee, Ch_mm, Ch_em, Ch_N };
-static string chanNames[] = {   "all",      "ee",  "mm",  "em"      };
+static std::string chanNames[] = {   "all",      "ee",  "mm",  "em"      };
 
 }
 }
--- SusyTest0/FakeLeptonSources.h
+++ SusyTest0/FakeLeptonSources.h
@@ -2,13 +2,13 @@
 #ifndef SUSY_FAKE_FAKELEPTONSOURCES_H
 #define SUSY_FAKE_FAKELEPTONSOURCES_H
 
-// Types and names of the possible sources of fake leptons
+#include <string>
 
 namespace susy {
 namespace fake {
 
-enum LeptonSource         { LS_HF = 0, LS_LF,   LS_Conv, LS_Real, LS_QCD, LS_Unk,  LS_N };
-static string LSNames[] = { "heavy",   "light", "conv",  "real",  "qcd", "unknown"      };
+enum LeptonSource              { LS_HF = 0, LS_LF,   LS_Conv, LS_Real, LS_QCD, LS_Unk,  LS_N };
+static std::string LSNames[] = { "heavy",   "light", "conv",  "real",  "qcd", "unknown"      };
 
 }
 }
