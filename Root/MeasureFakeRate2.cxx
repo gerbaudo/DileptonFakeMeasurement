f8021d38b1982ac2a90b47e621175d7e4843a30c
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-01-24 19:46:02 -0800

    update values and template with std-iso, ext-reg metrel>40 (Jan_21 tag)

--- Root/MeasureFakeRate2.cxx
+++ Root/MeasureFakeRate2.cxx
@@ -230,9 +230,9 @@ bool MeasureFakeRate2::passMCReg(const LeptonVector &leptons,
   if( leptons.size() != 2 ) return false;
   m_ch = SusySelection::getChan(leptons);
   m_metRel = getMetRel(met,leptons,jets);
-//  if(m_metRel<40.0) return false;
+  if(m_metRel<40.0) return false;
 //  if(jets.size()<1) return false;
-  if(jets.size()<1 || m_metRel<40.0) return false;
+//  if(jets.size()<1 || m_metRel<40.0) return false;
   for(uint il=0; il<leptons.size(); ++il){
     Lepton* l=leptons[il];
     bool isQcdLepton(susy::isHFLepton(l) || susy::isLFLepton(l));
--- run/python/buildWeightedMatrix.py
+++ run/python/buildWeightedMatrix.py
@@ -55,7 +55,8 @@ Example usage:
 """
 
 # scale factors from determineFakeScaleFactor.py
-# Jan_23, 2014-01-24 02:15:04.623855
+# --- paste the lines below in buildWeightedMatrix.py ---
+# Jan_21, 2014-01-24 15:10:16.575945
 mu_qcdSF, mu_realSF = 0.79, 0.99719
 el_convSF, el_qcdSF, el_realSF = 1.22, 0.73, 0.99728
 
