e199b6b2a4c1cdc806fe3814ba51649ea483441a
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-02-04 16:01:04 -0800

    SsPassFlags: add flags for Jan-optimized selections

--- SusyTest0/SsPassFlags.h
+++ SusyTest0/SsPassFlags.h
@@ -6,18 +6,17 @@
 struct SsPassFlags {
   SsPassFlags() { reset(); }
   void reset() {
-    eq2l = tauVeto = trig2l = trig2lmatch = true2l = sameSign = fjveto = bjveto = false;
+    eq2l = tauVeto = trig2l = trig2lmatch = true2l = sameSign = veto3rdL = fjveto = bjveto = false;
     ge1j = eq1j = ge2j = false;
-    lepPt = zllVeto = mtllmet = ht = metrel = veto3rdL = false;
+    lepPt = zllVeto = dEtall = maxMt = mljj = ht = metrel = mtllmet = false;
   }
   bool passLpt() const {
     return (eq2l & tauVeto & trig2l & trig2lmatch & true2l & sameSign
-            & fjveto & bjveto & ge1j & lepPt);
+            & veto3rdL & fjveto & bjveto & ge1j & lepPt);
   }
-  bool passAll() const { return (passLpt() & zllVeto & mtllmet & ht & metrel & veto3rdL); }
-  bool eq2l, tauVeto, trig2l, trig2lmatch, true2l, sameSign, fjveto, bjveto;
+  bool eq2l, tauVeto, trig2l, trig2lmatch, true2l, sameSign, veto3rdL, fjveto, bjveto;
   bool ge1j, eq1j, ge2j;
-  bool lepPt, zllVeto, mtllmet, ht, metrel, veto3rdL;
+  bool lepPt, zllVeto, dEtall, maxMt, mljj, ht, metrel, mtllmet;
   std::string str() const;
 };
 
