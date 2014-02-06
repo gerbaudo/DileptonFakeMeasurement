2ea48e685c02f5b3f2892274d0cc64e65c49d0a9
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-02-06 12:22:41 -0800

    getBTagWeight : Jet::Eta -> detEta

--- Root/SusySelection.cxx
+++ Root/SusySelection.cxx
@@ -454,7 +454,7 @@ float SusySelection::getBTagWeight(cvj_t& jets, const Event* evt)
   JetVector tempJets;
   for(uint ij=0; ij<jets.size(); ++ij){
     Jet* jet = jets.at(ij);
-    if( !(jet->Pt() > 20 && fabs(jet->Eta()) < JET_ETA_CUT_2L) ) continue;
+    if( !(jet->Pt() > 20 && fabs(jet->detEta) < JET_ETA_CUT_2L) ) continue;
     tempJets.push_back(jet);
   }
   return bTagSF(evt, tempJets, evt->mcChannel, BTag_NOM);
