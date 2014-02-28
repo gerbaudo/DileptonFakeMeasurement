8ef4b9dd014285bce8fb0a85a4a9f79cad4c3444
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-02-27 16:44:46 -0800

    add flavor binning (now used several times w/ additional 2d histos)

--- SusyTest0/FakeBinnings.h
+++ SusyTest0/FakeBinnings.h
@@ -12,12 +12,14 @@ const float Etabins[]          = {0,1.37,1.52,2.1,2.5};
 const float CoarseEtabins[]    = {0,1.37,2.5};
 const float Metbins[]          = {0, 20, 30, 40, 50, 60, 100, 200};
 const float Jetbins[]          = {-0.5, 0.5, 1.5, 2.5, 3.5};
+const float flavBins[]         = {-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5};
 const int   nFakePtbins        = sizeof(FakePtbins)       / sizeof(FakePtbins[0])       - 1;
 const int   nCoarseFakePtbins  = sizeof(coarseFakePtbins) / sizeof(coarseFakePtbins[0]) - 1;
 const int   nEtabins           = sizeof(Etabins)          / sizeof(Etabins[0])          - 1;
 const int   nCoarseEtabins     = sizeof(CoarseEtabins)    / sizeof(CoarseEtabins[0])    - 1;
 const int   nMetbins           = sizeof(Metbins)          / sizeof(Metbins[0])          - 1;
 const int   nJetbins           = sizeof(Jetbins)          / sizeof(Jetbins[0])          - 1;
+const int   nFlavBins          = sizeof(flavBins)         / sizeof(flavBins[0])         - 1;
 } // end namespace fake
 } // end namespace susy
 
