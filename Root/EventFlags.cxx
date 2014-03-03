2224971a22f4e1fc6dc6f471b6ef93f7dba6f60d
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-03-03 12:54:46 -0800

    EventFlags::str: add spaces between tokens

--- Root/EventFlags.cxx
+++ Root/EventFlags.cxx
@@ -8,21 +8,21 @@ using susy::wh::EventFlags;
 std::string EventFlags::str() const
 {
   std::ostringstream oss;
-  oss<<"grl: "           <<grl
-     <<"larErr: "        <<larErr
-     <<"tileErr: "       <<tileErr
-     <<"ttcVeto: "       <<ttcVeto
-     <<"goodVtx: "       <<goodVtx
-     <<"tileTrip: "      <<tileTrip
-     <<"lAr: "           <<lAr
-     <<"badJet: "        <<badJet
-     <<"deadRegions: "   <<deadRegions
-     <<"badMuon: "       <<badMuon
-     <<"cosmicMuon: "    <<cosmicMuon
-     <<"hfor: "          <<hfor
-     <<"ge2blep: "       <<ge2blep
-     <<"eq2blep: "       <<eq2blep
-     <<"mllMin: "        <<mllMin;
+  oss<<" grl: "           <<grl
+     <<" larErr: "        <<larErr
+     <<" tileErr: "       <<tileErr
+     <<" ttcVeto: "       <<ttcVeto
+     <<" goodVtx: "       <<goodVtx
+     <<" tileTrip: "      <<tileTrip
+     <<" lAr: "           <<lAr
+     <<" badJet: "        <<badJet
+     <<" deadRegions: "   <<deadRegions
+     <<" badMuon: "       <<badMuon
+     <<" cosmicMuon: "    <<cosmicMuon
+     <<" hfor: "          <<hfor
+     <<" ge2blep: "       <<ge2blep
+     <<" eq2blep: "       <<eq2blep
+     <<" mllMin: "        <<mllMin;
   return oss.str();
 }
 //-----------------------------------------
