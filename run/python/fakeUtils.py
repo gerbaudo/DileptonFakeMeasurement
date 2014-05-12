6f8c144872755937210efe88ce49f5feedabf7f6
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-05-12 00:15:48 -0700

    fakeu: elecIsTight, also require tightPP

--- run/python/fakeUtils.py
+++ run/python/fakeUtils.py
@@ -85,14 +85,17 @@ def plot2dEfficiencies(effs={}, canvasName='', outputDir='./', frameTitle='effic
     r.gStyle.SetPaintTextFormat(origTextFormat)
 
 #___________________________________________________________
-def isTight_std(l): return electronIsTight_std(l) if l.isEl else muonIsTight_std(l) if isMu else False
-def isTight_wh (l): return electronIsTight_wh (l) if l.isEl else muonIsTight_wh (l) if isMu else False
+def isTight_std(l):   return elecIsTight_std  (l) if l.isEl else muonIsTight_std  (l) if l.isMu else False
+def isTight_tight(l): return elecIsTight_tight(l) if l.isEl else muonIsTight_tight(l) if l.isMu else False
+def isTight_wh (l):   return elecIsTight_wh   (l) if l.isEl else muonIsTight_wh   (l) if l.isMu else False
 
-def elecIsTight_std(l): return elecIsFromPv(l) and elecIsIsolated(l, denominator_std(l), etConeThres=0.18, ptConeThres=0.16)
-def elecIsTight_wh (l): return elecIsFromPv(l) and elecIsIsolated(l, denominator_wh (l), etConeThres=0.13, ptConeThres=0.07)
+def elecIsTight_std  (l): return l.isTightPp and elecIsFromPv(l) and elecIsIsolated(l, denominator_std(l), etConeThres=0.18, ptConeThres=0.16)
+def elecIsTight_tight(l): return l.isTightPp and elecIsFromPv(l) and elecIsIsolated(l, denominator_std(l), etConeThres=0.13, ptConeThres=0.07)
+def elecIsTight_wh   (l): return l.isTightPp and elecIsFromPv(l) and elecIsIsolated(l, denominator_wh (l), etConeThres=0.13, ptConeThres=0.07)
 
-def muonIsTight_std(l): return muonIsFromPv(l) and muonIsIsolated(l, denominator_std(l), etConeThres=None, ptConeThres=0.12)
-def muonIsTight_wh (l): return muonIsFromPv(l) and muonIsIsolated(l, denominator_wh (l), etConeThres=0.14, ptConeThres=0.06)
+def muonIsTight_std  (l): return muonIsFromPv(l) and muonIsIsolated(l, denominator_std(l), etConeThres=None, ptConeThres=0.12)
+def muonIsTight_tight(l): return muonIsFromPv(l) and muonIsIsolated(l, denominator_std(l), etConeThres=0.14, ptConeThres=0.06)
+def muonIsTight_wh   (l): return muonIsFromPv(l) and muonIsIsolated(l, denominator_wh (l), etConeThres=0.14, ptConeThres=0.06)
 
 def denominator_wh (l) :
     pt = l.p4.Pt()
@@ -102,15 +105,15 @@ def denominator_std(l) :
     pt = l.p4.Pt()
     return 1.0/pt if pt>0.0 else None
 
-def electronIsFromPv(l):
+def elecIsFromPv(l):
     maxD0Sig, maxZ0SinTheta = 3.0, 0.4 # see SusyNtTools::isSignalElectron
     return abs(l.d0Signif) < maxD0Sig and abs(l.z0SinTheta) < maxZ0SinTheta
 
 def muonIsFromPv(l):
     maxD0Sig, maxZ0SinTheta = 3.0, 1.0 # see SusyNtTools::isSignalMuon
-    return abs(l.d0Sig) < maxD0Sig and abs(l.z0SinTheta) < maxZ0SinTheta
+    return abs(l.d0Signif) < maxD0Sig and abs(l.z0SinTheta) < maxZ0SinTheta
 
-def electronIsIsolated(l, denom, etConeThres, ptConeThres):
+def elecIsIsolated(l, denom, etConeThres, ptConeThres):
     pt, etCone, ptCone = l.p4.Pt(), l.etConeCorr, l.ptConeCorr
     return ((etCone*denom < etConeThres if etConeThres else True) and
             (ptCone*denom < ptConeThres if ptConeThres else True)
@@ -118,8 +121,8 @@ def electronIsIsolated(l, denom, etConeThres, ptConeThres):
 
 def muonIsIsolated(l, denom, etConeThres, ptConeThres):
     pt, etCone, ptCone = l.p4.Pt(), l.etConeCorr, l.ptConeCorr # DG: Nt uses ptConeCorr only for 3lep?
-    return ((etCone*denom < etConeThres if etConeThres) and
-            (ptCone*denom < ptConeThres if ptConeThres)
+    return ((etCone*denom < etConeThres if etConeThres else True) and
+            (ptCone*denom < ptConeThres if ptConeThres else True)
             if denom else False)
 #___________________________________________________________
 # see SusyDefs.h:TrigBit
