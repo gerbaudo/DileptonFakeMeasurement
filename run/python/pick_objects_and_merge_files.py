11c4761f83f177512c095cf57c3cb1a8c0d8f3fb
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-08-11 10:00:55 -0700

    pick&merge: use new Jul_26 files

--- run/python/pick_objects_and_merge_files.py
+++ run/python/pick_objects_and_merge_files.py
@@ -8,19 +8,16 @@ r.gROOT.SetBatch(1)
 
 input0 = '/gdata/atlas/gerbaudo/wh/Susy2013_Nt_01_04/SusyMatrixMethod/data/FinalFakeHist_Apr_10.root'
 basedir = '/gdata/atlas/gerbaudo/wh/Susy2013_Nt_01_04_fake_dev/SusyTest0/run'
-input1 = basedir+'/'+'out/fake_matrix_May_10/fake_matrices_el.root'
-input2 = basedir+'/'+'out/fake_matrix_May_10/fake_matrices_mu.root'
-input3 = basedir+'/'+'out/fake_matrix_May_10/fake_matrices_el_anygroup.root'
-input4 = basedir+'/'+'out/fake_matrix_May_10/fake_matrices_mu_anygroup.root'
-input5 = basedir+'/'+'out/fake_matrix_syst_shift_May_10/fake_matrices_el.root'
-input6 = basedir+'/'+'out/fake_matrix_syst_shift_May_10/fake_matrices_mu.root'
 
-outfileName = '/gdata/atlas/gerbaudo/wh/Susy2013_Nt_01_04/SusyMatrixMethod/data/FinalFakeHist_May_20.root'
+input1 = basedir+'/'+'out/fake/weigtedmatrix_Jul_26/fake_matrices_el.root'
+input2 = basedir+'/'+'out/fake/weigtedmatrix_Jul_26/fake_matrices_mu.root'
 
-objMap = { input0 : [('mu_real_eff_CR_SSInc1j', None) # none = keep the orig name
-                     ,('mu_real_eff2d_CR_SSInc1j', None)
-                     ,('el_real_eff_CR_SSInc1j', None)
-                     ,('el_real_eff2d_CR_SSInc1j', None)
+outfileName = '/gdata/atlas/gerbaudo/wh/Susy2013_Nt_01_04/SusyMatrixMethod/data/FinalFakeHist_Jul_26.root'
+#('orig_name', 'new_name')  # none = keep the orig name
+objMap = { input0 : [('mu_real_eff_CR_SSInc1j',    'mu_real_eff_emuInc')
+                     ,('mu_real_eff2d_CR_SSInc1j', 'mu_real_eff2d_emuInc')
+                     ,('el_real_eff_CR_SSInc1j',   'el_real_eff_emuInc')
+                     ,('el_real_eff2d_CR_SSInc1j', 'el_real_eff2d_emuInc')
                      ,('el_real_up', None)
                      ,('el_real_down', None)
                      ,('mu_real_up', None)
@@ -33,21 +30,13 @@ objMap = { input0 : [('mu_real_eff_CR_SSInc1j', None) # none = keep the orig nam
                      ,('mu_region', None)
                      ,('el_eta_sys', None)
                      ,('mu_eta_sys', None)
-                     ,('el_fake_rate_CR_SSInc1j', None) # put it there, but we shouldn't use the 1d param anymore...
-                     ,('mu_fake_rate_CR_SSInc1j', None)
-                     ,('el_real_eff2d_CR_SRWHnoMlj', None)
-                     ,('el_fake_rate2d_CR_SRWHnoMlj', None)
-                     ,('mu_real_eff2d_CR_SRWHnoMlj', None)
-                     ,('mu_fake_rate2d_CR_SRWHnoMlj', None)
                      ],
-           input1 : [('el_fake_pt_eta_ssinc1j', 'el_fake_rate2d_CR_SSInc1j')
+           input1 : [('el_fake_pt_emu',      'el_fake_rate_emuInc')
+                     ,('el_fake_pt_eta_emu', 'el_fake_rate2d_emuInc')
                      ],
-           input2 : [('mu_fake_pt_eta_ssinc1j', 'mu_fake_rate2d_CR_SSInc1j')
+           input2 : [('mu_fake_pt_emu',      'mu_fake_rate_emuInc')
+                     ,('mu_fake_pt_eta_emu', 'mu_fake_rate2d_emuInc')
                      ],
-           input3 : [('el_fake_pt_eta_ssinc1j', 'el_fake_rate2d_CR_SSInc1j_frac_do')],
-           input4 : [('mu_fake_pt_eta_ssinc1j', 'mu_fake_rate2d_CR_SSInc1j_frac_do')],
-           input5 : [('el_fake_pt_eta_ssinc1j', 'el_fake_rate2d_CR_SSInc1j_frac_up')],
-           input6 : [('mu_fake_pt_eta_ssinc1j', 'mu_fake_rate2d_CR_SSInc1j_frac_up')],
            }
 inputFiles = dict((fn, r.TFile.Open(fn, 'read')) for fn in objMap.keys())
 outputFile = r.TFile.Open(outfileName, 'recreate')
